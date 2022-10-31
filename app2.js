8d98ec4c33ae33e74cde15650a28542d6dc75fcc
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-31 09:13:33 -0400

    process obj

--- app2.js
+++ app2.js
@@ -2,3 +2,12 @@ console.log(__dirname);
 console.log(__filename);
 //console.log(process);
 console.log(process.memoryUsage()); //define memory usage for app
+console.log(process.argv);
+console.log(process.pid);
+//console.log(process.argv[1]);
+
+const first = process.argv[2];
+const second = process.argv[3];
+
+let message = `${first} ${second}`;
+console.log(message);
