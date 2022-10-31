4cdede39e73260d2e7686dd776fe53be3b969002
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-31 09:42:22 -0400

    using process obj

--- app3.js
+++ app3.js
@@ -1,1 +1,12 @@
-console.log(process);
+//console.log(process); //global object
+
+//console.log(process.pid);
+//console.log(process.argv);
+
+
+const first = process.argv[2]; // place first name as argument
+const second = process.argv[3]; // place second name as argument 
+
+let message = `Hello, welcome ${first} ${second}`;
+console.log(message);
+
