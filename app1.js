484fd9b3a877bf5d3fbc48a39ec48e2f33473a50
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-31 08:39:12 -0400

    praise

--- app1.js
+++ app1.js
@@ -1,7 +1,7 @@
 console.log('Hello from app1.js');
 for(let x=0; x<50000;x++) {
-    console.log('I Love Jackie'); // change output from number to words
-    if(x==30000) {
-        process.exit() //cause exit
-    }
+    console.log('Praise G_D !'); // change output from number to words
+    //if(x==30000) {
+       // process.exit() //cause exit
+   // }
 }
