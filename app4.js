3f66e9dacc4dce7518705c58e91087cd0cefadba
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-31 10:02:40 -0400

    use nodemon and count function

---
+++ app4.js
@@ -0,0 +1,15 @@
+// setup nodemon for changes
+// npm i -g nodemon
+// nodemon aap.js
+// contol c to exit
+
+console.log('Hi, Jacqueline Taylor Hendricks');
+console.log('hello RMH');
+const a = 4;
+console.log(a);
+
+console.log('%s', a)
+console.count('test',a) //using count function
+console.count('test',a)
+console.count('test',a)
+console.count('test',a)
