3eb9b1f5b2b5828b93d4af076ff0ee4acc9d0953
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-10-31 08:37:39 -0400

    say someting

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 console.log('Hello from app1.js');
 for(let x=0; x<50000;x++) {
-    console.log(x);
+    console.log('I Love Jackie'); // change output from number to words
     if(x==30000) {
         process.exit() //cause exit
     }
